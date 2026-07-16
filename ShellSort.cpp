0caddaa2b4d2e187f731a8743e422c49286d7ea7
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-07-16 11:02:33 +0700

    menambahkan library

---
+++ ShellSort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int n;        // Menyimpan jumlah elemen aktif yang diinput user
+int arr[20];  // Array statis langsung dipesan dengan kapasitas maksimal 20 elemen
